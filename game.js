14d7fd1b9af679b9be42d46f6dde4f1fc7a78bef
Davids Levs Zvirgzds <davids.levs@gmail.com>
2024-02-20 23:49:45 +0200

    Major refactor, implement UI

--- game.js
+++ game.js
@@ -1,33 +1,25 @@
 // Rock Paper Scissors game
 const possibleChoices = ['scissors', 'rock', 'paper']; 
+let gameActive = false;
+let currentRound = 0;
+let scores = {player: 0, computer: 0}
 
 // Returns a random choice
 function getComputerChoice() {
     return possibleChoices[Math.floor(Math.random() * possibleChoices.length)]
 }
 
-// Safely returns the player's choice
-function getPlayerChoice(invalidMsg) {
-    let promptMsg = invalidMsg ? 'Invalid choice! Please try again.' : 'Please choose your weapon!';
-    let playerChoice = prompt(promptMsg);
-    if (possibleChoices.includes(playerChoice.toLowerCase())) { 
-        return playerChoice;
-    } else {
-        return getPlayerChoice(true);
-    }
-}
-
 // Returns the result of a round using the given parameters
-function playRound(playerSelection, computerSelection) {
+function getRoundResults(playerSelection) {
     // Credit: https://stackoverflow.com/questions/53730900/more-efficient-choice-comparison-for-rock-paper-scissors
     // Order the array so that the winner is always to the right of the given choice
     // Therefore, we can easily check if the index is to the right for a winner, and anywhere else for a tie/lose
     const playerSelectionProcessed = playerSelection.toLowerCase();
     const uIdx = possibleChoices.indexOf(playerSelectionProcessed); // User's index
-    const mIdx = possibleChoices.indexOf(computerSelection);
+    const mIdx = possibleChoices.indexOf(getComputerChoice());
     let winner;
     if (uIdx !== mIdx) {
-        winner = (mIdx === (uIdx + 1) % 3) ? 'loss' : 'win';
+        winner = (mIdx === (uIdx + 1) % 3) ? 'computer' : 'player';
         // Pick 'You Lost' if the index of the computer's selection is to the right of the user's selection.
         // We use modulus so we can 'wrap around' the array if the choice is all the way at the ened of the array.
     } else {
@@ -36,28 +28,69 @@ function playRound(playerSelection, computerSelection) {
     return winner;
 }
 
-// Plays 5 rounds, keeps a score, and declares the winner of the game at the end
-function playGame() {
-    let machineScore = 0;
-    let playerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        const computerSelection = getComputerChoice();
-        const playerSelection = getPlayerChoice();
-        const winState = playRound(playerSelection, computerSelection);
-        if (winState !== 'tie') {
-            machineScore = (winState === 'loss') ? machineScore + 1 : machineScore;
-            playerScore = (winState === 'win') ? playerScore + 1 : playerScore;
-        } else if (winState === 'tie') {
-            machineScore++;
-            playerScore++;
-        }
-        console.log("Your score: " + playerScore.toString() + " | The computer's score: " + machineScore.toString());
-    }
-    if (machineScore !== playerScore) {
-        const msgPrefix = (machineScore > playerScore) ? 'You lost!' : 'You won!';
-        console.log(msgPrefix);
-    } else {
-        console.log("Tie!");
-    }
-    console.log("Your score: " + playerScore.toString() + " | The computer's score: " + machineScore.toString());
+const playerScoreText = document.querySelector('#playerScore');
+const computerScoreText = document.querySelector('#computerScore');
+function displayCurrentScore() {
+  playerScoreText.innerText = 'your score: ' + scores.player;
+  computerScoreText.innerText = 'computer score ' + scores.computer;
 }
+
+const currentRoundText = document.querySelector('#currentRound')
+function displayCurrentRound() {
+  currentRoundText.innerText = 'round: ' + currentRound;
+}
+
+const gameResultsText = document.querySelector('#gameResults');
+function showWinner() {
+  if (scores.player > scores.computer) {
+    gameResultsText.innerText = 'you won!';
+  } else if (scores.player < scores.computer) {
+    gameResultsText.innerText = 'you lost!';
+  } else {
+    gameResultsText.innerText = 'tie!';
+  }
+}
+
+// end the current game and display the score
+function endGame() {
+  gameActive = false;
+  currentRound = 0;
+  showWinner();
+}
+
+// Plays a round, adds to the score
+function playRound(playerSelection) {
+  if (!gameActive) return;
+  currentRound += 1
+  let roundResult = getRoundResults(playerSelection);
+  scores.player += (roundResult === 'tie' || roundResult === 'player') ? 1 : 0;
+  scores.computer += (roundResult === 'tie' || roundResult === 'computer') ? 1 : 0;
+
+  displayCurrentScore();
+  displayCurrentRound();
+
+  if (currentRound >= 5) {
+    endGame();
+  }
+}
+
+function playGame() {
+  gameActive = true;
+  scores.player = 0;
+  scores.computer = 0;
+  currentRound = 0;
+
+  gameResultsText.innerText = '';
+  displayCurrentScore();
+  displayCurrentRound();
+}
+
+const playGameBtn = document.querySelector('#playGame');
+playGameBtn.addEventListener('click', () => playGame());
+
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+rockBtn.addEventListener('click', () => playRound('rock'));
+paperBtn.addEventListener('click', () => playRound('paper'));
+scissorsBtn.addEventListener('click', () => playRound('scissors'));
